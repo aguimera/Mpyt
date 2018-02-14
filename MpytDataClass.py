7b02ba375299b51e7ac6fa07acb03e8928f33b86
aguimera <anton.guimera@csic.es>
2018-02-15 00:44:08 +0100

    Add cycle reader

--- MpytDataClass.py
+++ MpytDataClass.py
@@ -70,15 +70,102 @@ class MpytDataBase(object):
             self.__setattr__(prop, v)
         self.__setattr__('Units', Units)
 
+        self.GetCPcycles()
+        self.GetPIEScycles()
+        self.GetCVcycles()
+
+    def GetCVcycles(self):
+        if 'ox' not in self.__dict__.keys():
+            self.CVcycles = []
+            return
+
+        nCycles = set(self.cyclenumber)
+        CVcycles = []
+        for cy in nCycles:
+            inds = np.where(self.cyclenumber == cy)
+            Cycle = {'time': self.time[inds] - self.time[inds[0][0]],
+                     'I': self.I[inds],
+                     'Ewe': self.Ewe[inds],
+                     'Cycle': cy}
+            CVcycles.append(Cycle)
+
+        self.CVcycles = CVcycles
+
+    def GetCPcycles(self, DTimeSplitter=1):
+        if self.mode[0] != 1:
+            self.CPcycles = []
+            return
+
+        dt = np.diff(self.time)
+        sepInds = np.where(dt > DTimeSplitter)[0]
+        if len(sepInds) == 0:
+            sepInds = (0, )
+        CPcycles = []
+        oldInd = -1
+        for cy, sep in enumerate(sepInds):
+            inds = range(oldInd+1, sep)
+            time = self.time[inds]
+            time = time - time[0]
+            oldInd = sep
+            Cycle = {'time': time,
+                     'I': self.I[inds],
+                     'Ewe': self.Ewe[inds],
+                     'Cycle': cy}
+            CPcycles.append(Cycle)
+
+        self.CPcycles = CPcycles
+
+    def GetPIEScycles(self, DTimeSplitter=100):
+        if 'ReZ' not in self.__dict__.keys():
+            self.PIEScycles = []
+            return
+
+        dt = np.diff(self.time)
+        sepInds = np.where(dt > DTimeSplitter)[0]
+        if len(sepInds) == 0:
+            sepInds = (0, )
+        PIEScycles = []
+        oldInd = -1
+        for cy, sep in enumerate(sepInds):
+            inds = range(oldInd+1, sep)
+            time = self.time[inds]
+            time = time - time[0]
+            oldInd = sep
+            finds = np.where(self.freq[inds] > 0)[0]
+            Cycle = {'time': time,
+                     'Ze': (self.ReZ[inds][finds] - self.ImZ[inds][finds]*1j),
+                     'freq': self.freq[inds][finds],
+                     'Cycle': cy}
+            PIEScycles.append(Cycle)
+
+        self.PIEScycles = PIEScycles
 
 if __name__ == "__main__":
+    
+    plt.close('all')
 
 #    FileName = './TestFiles/B5_Cor64_D1_Eup4_M12_01_CV.mpt'
-    FileName = './TestFiles/B12_MEA1_57_13_B2_M14_Pulses_1M_8mCcm2_04_PEIS.mpt'
-#    FileName = 'B12_MEA1_57_13_B2_M14_Pulses_1M_8mCcm2_02_CP Fast.mpt'
-    
+#    FileName = './TestFiles/B12_MEA1_57_13_B2_M14_Pulses_1M_8mCcm2_04_PEIS.mpt'
+    FileName = './TestFiles/B12_MEA1_57_13_B2_M14_Pulses_1M_8mCcm2_02_CP Fast.mpt'
+#    FileName = './TestFiles/B12_MEA1_57_13_B2_M2_Pulses_05_CP Fast.mpt'
+       
     MpytData = MpytDataBase(FileName=FileName)
     
+    print MpytData.__dict__.keys()
     
-    
-    
+    fig, (Axm, Axp) = plt.subplots(2, 1, sharex=True)
+    for IES in MpytData.PIEScycles:
+        Axm.loglog(IES['freq'], np.abs(IES['Ze']))
+        Axp.semilogx(IES['freq'], np.angle(IES['Ze'], deg=True))
+
+    fig, Axcv = plt.subplots()
+    for CV in MpytData.CVcycles:
+        Axcv.plot(CV['Ewe'], CV['I'])
+
+    fig, AxcpE = plt.subplots()
+    AxcpI = plt.twinx(AxcpE)
+    for CP in MpytData.CPcycles:
+        AxcpE.plot(CP['time'], CP['Ewe'])
+        AxcpI.plot(CP['time'], CP['I'],'--')
+
+
