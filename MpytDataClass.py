1dcf7ef6b4bbce54a50a041abdb00dbc698d3fbd
aguimera <anton.guimera@csic.es>
2018-02-19 16:55:29 +0100

    add quantities

--- MpytDataClass.py
+++ MpytDataClass.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools as it
 import pandas as pd
+import quantities as pq
 
 
 def TextFileReader(FileName):
@@ -52,14 +53,16 @@ def TextFileReader(FileName):
     return Data
 
 
-def CalcScanRate(time, V):
+def CalcScanRate(time, V, DevRem=0.25):
     SR = np.abs(np.diff(V)/np.diff(time))
-    SR = SR[np.abs(SR - np.mean(SR)) < 0.25 * np.std(SR)]
+    SR = SR[np.abs(SR - np.mean(SR)) < DevRem * np.std(SR)]
     return np.mean(SR)
 
 
 class MpytDataBase(object):
     PropRemoveChars = ('-', '(', ')', '<', '>', ' ', '.', '|')
+    SRDevRemoval = 0.25    
+    Area = 5e-6*pq.cm**2
 
     def __init__(self, FileName, DataDict=None):
         if DataDict is None:
@@ -71,9 +74,12 @@ class MpytDataBase(object):
             prop = k[0]
             for char in self.PropRemoveChars:
                 prop = prop.replace(char, '')
-            if len(k) > 1:
+            if (len(k) > 1) and (prop != 'ox'):
                 Units.update({prop: k[1]})
-            self.__setattr__(prop, v)
+                vq = pq.Quantity(v, units=k[1])
+                self.__setattr__(prop, vq)
+            else:
+                self.__setattr__(prop, v)
         self.__setattr__('Units', Units)
 
         self.GetCPcycles()
@@ -88,30 +94,49 @@ class MpytDataBase(object):
 
         Cap = []
         for cv in self.CVcycles:
-            I = cv['I']
-            V = cv['Ewe']
+            Ie = cv['I']
+            Ve = cv['Ewe']
             time = cv['time']
 
-            SR = CalcScanRate(time, V)       
-            PotWin = (np.min(V), np.max(V))
-            CenterPotWin = PotWin[1] + PotWin[0] #center of PW TODO !!!! Could crash if have same sign
-
-            Ind1 = np.min(np.where(V < CenterPotWin)[0])            
-            Ind2 = np.min(np.where(V[Ind1:] > CenterPotWin)[0])
-
-#            print Ind1, Ind2
-#            print CenterPotWin
-
-            ICap = np.mean(np.abs(I[[Ind1, Ind2]]))            
-            cv['Cap'] = ICap*SR
-            Cap.append(cv['Cap'])
-            
+            # calc SR, PotWin, and CenterPotWin
+            SR = CalcScanRate(time, Ve, DevRem=self.SRDevRemoval)
+            PotWin = (np.min(Ve), np.max(Ve))
+            CenterPotWin = PotWin[1] + PotWin[0]  # center of PW
+            # TODO !!!! Could crash if have same sign
             cv['SR'] = SR
             cv['PotWin'] = PotWin
-            cv['CenterPotWin'] = CenterPotWin             
-        self.Cap = Cap
-    
-        
+            cv['CenterPotWin'] = CenterPotWin
+
+
+            # Calc Capacitance
+            Ind1 = np.min(np.where(Ve < CenterPotWin)[0])
+            Ind2 = np.min(np.where(Ve[Ind1:] > CenterPotWin)[0])
+
+            ICap = np.mean(np.abs(Ie[[Ind1, Ind2]]))
+            cv['Cap'] = ICap/SR
+            Cap.append(cv['Cap'])
+
+            # Calc CSC
+            Irange1 = np.min(np.abs(Ie[np.where(Ve < 0)[0]]))
+            Irange2 = np.min(np.abs(Ie[np.where(Ve > 0)[0]]))
+            Indcat = np.where(Ie <= Irange1)[0]
+            Indan = np.where(Ie >= Irange2)[0]
+            cv['Indcat'] = Indcat
+            cv['Indan'] = Indan
+            CSCcat0 = np.trapz(Ie[Indcat], Ve[Indcat])
+            CSCan0 = np.trapz(Ie[Indan], Ve[Indan])
+            cv['CSCcat0'] = CSCcat0
+            cv['CSCan0'] = CSCan0
+            cv['CSCcat'] = (CSCcat0 / SR).rescale('C')
+            cv['CSCan'] = (CSCan0 / SR)
+
+        if len(Cap) > 0:
+            self.Cap = (np.mean(Cap[1:])*Cap[-1].units).rescale('mF')
+            self.CapErr = self.Cap/np.std(Cap[1:])
+        else:
+            self.Cap = (np.mean(Cap)*Cap[-1].units).rescale('mF')
+            self.CapErr = None
+
     def GetCVcycles(self):
         if 'ox' not in self.__dict__.keys():
             self.CVcycles = []
@@ -178,42 +203,61 @@ class MpytDataBase(object):
 
         self.PIEScycles = PIEScycles
 
+
+    def GetCap(self, units='mF/cm**2'):
+        return (self.Cap/self.Area).rescale(units)
+
 if __name__ == "__main__":
     
     plt.close('all')
 
-    FileName = './TestFiles/B5_Cor64_D1_Eup4_M12_01_CV.mpt'
-#    FileName = './TestFiles/B12_MEA1_57_13_B2_M14_Pulses_1M_8mCcm2_04_PEIS.mpt'
-#    FileName = './TestFiles/B12_MEA1_57_13_B2_M14_Pulses_1M_8mCcm2_02_CP Fast.mpt'
-#    FileName = './TestFiles/B12_MEA1_57_13_B2_M2_Pulses_05_CP Fast.mpt'
-       
-    MpytData = MpytDataBase(FileName=FileName)
-    
-    print MpytData.__dict__.keys()
+#    FileName = './TestFiles/B5_Cor64_D1_Eup4_M12_01_CV.mpt'
+##    FileName = './TestFiles/B12_MEA1_57_13_B2_M14_Pulses_1M_8mCcm2_04_PEIS.mpt'
+##    FileName = './TestFiles/B12_MEA1_57_13_B2_M14_Pulses_1M_8mCcm2_02_CP Fast.mpt'
+##    FileName = './TestFiles/B12_MEA1_57_13_B2_M2_Pulses_05_CP Fast.mpt'
+#       
+#    MpytData = MpytDataBase(FileName=FileName)
+#    
+#    print MpytData.__dict__.keys()
     
 #    fig, (Axm, Axp) = plt.subplots(2, 1, sharex=True)
 #    for IES in MpytData.PIEScycles:
 #        Axm.loglog(IES['freq'], np.abs(IES['Ze']))
 #        Axp.semilogx(IES['freq'], np.angle(IES['Ze'], deg=True))
 #
+    
+###############################################################################
+### CSC debug plots
+###############################################################################  
+    FileName = './TestFiles/B5_Cor64_D1_Eup4_M12_01_CV.mpt'
+    MpytData = MpytDataBase(FileName=FileName)
+
     fig, Axcv = plt.subplots()    
     for CV in MpytData.CVcycles:
         Axcv.plot(CV['Ewe'], CV['I'])
-    plt.figure()
+        inds = CV['Indcat']
+        Axcv.plot(CV['Ewe'][inds], CV['I'][inds],'*')
+        inds = CV['Indan']
+        Axcv.plot(CV['Ewe'][inds], CV['I'][inds],'+k')
+        
     print MpytData.Cap
-    
+    fig, AxV = plt.subplots()
+    AxI = plt.twinx(AxV)
+    plt.figure()
+    AxV.plot(CV['Ewe'])
+    AxI.plot(CV['I'])
 
-#    fig, AxcpE = plt.subplots()
-#    AxcpI = plt.twinx(AxcpE)
-#    for CP in MpytData.CPcycles:
-#        AxcpE.plot(CP['time'], CP['Ewe'])
-#        AxcpI.plot(CP['time'], CP['I'],'--', alpha=0.5)
+    fig, AxcpE = plt.subplots()
+    AxcpI = plt.twinx(AxcpE)
+    for CP in MpytData.CPcycles:
+        AxcpE.plot(CP['time'], CP['Ewe'])
+        AxcpI.plot(CP['time'], CP['I'],'--', alpha=0.5)
 
 ###############################################################################
 ### SR call debug
 ###############################################################################    
-    ## SR debugging
-    ## direct option
+    # SR debugging
+    # direct option
 #    FileName = './TestFiles/B5_Cor64_D1_Eup4_M12_01_CV.mpt'
 #    MpytData = MpytDataBase(FileName=FileName)
 #    fig, Ax1 = plt.subplots()
